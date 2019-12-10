52b7bf5a2e2fbc6a4f1f8f3ca286012e98633917
Hanh Nguyen <hanhuyeny2k@gmail.com>
2019-12-10 02:19:56 +0000

    Fixed indentation errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #Using Puppet to create a file in /tmp.
 
-file {
-  '/tmp/holberton':
-  owner => 'www-data',
-  group => 'www-data',
-  mode => '0744',
+file { '/tmp/holberton':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
   content => 'I love Puppet',
 }
