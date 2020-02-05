b424ffffb3374762e3b24fc92bcb96d0e2bbac4c
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-05 22:07:44 +0000

    added if name main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,20 +4,22 @@
 import requests
 from sys import argv
 
-NUMBER_OF_DONE_TASKS = 0
-t = []
-user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                    .format(argv[1])).json()
-todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                     .format(argv[1])).json()
-TOTAL_OF_TASKS_DONE = len(todos)
-for i in todos:
-    if i.get('completed') is True:
-        NUMBER_OF_DONE_TASKS += 1
-        q = i.get('title')
-        t.append(q)
-TODO = "Employee {} is done with tasks({}/{}):".format(
-        user.get('name'), NUMBER_OF_DONE_TASKS, TOTAL_OF_TASKS_DONE)
-print(TODO)
-for i in t:
-    print('\t {}'.format(i))
+if __name__ == "__main__":
+
+    NUMBER_OF_DONE_TASKS = 0
+    t = []
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1])).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(argv[1])).json()
+    TOTAL_OF_TASKS_DONE = len(todos)
+    for i in todos:
+        if i.get('completed') is True:
+            NUMBER_OF_DONE_TASKS += 1
+            q = i.get('title')
+            t.append(q)
+    TODO = "Employee {} is done with tasks({}/{}):".format(
+            user.get('name'), NUMBER_OF_DONE_TASKS, TOTAL_OF_TASKS_DONE)
+    print(TODO)
+    for i in t:
+        print('\t {}'.format(i))
