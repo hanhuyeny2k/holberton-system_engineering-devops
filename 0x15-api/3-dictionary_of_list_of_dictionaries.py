811244fdb12f7c78974602cb2dd177f8cf282e0a
Hanh Nguyen <hanhuyeny2k@gmail.com>
2020-02-06 00:05:35 +0000

    fixed indentation error

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             if j.get('id') == i.get('userId'):
                 q = {"username": j.get('username'), "task": i.get('title'),
                      "completed": i.get('completed')}
-            task_list.append(q)
-    todos_dict[j.get('id')] = task_list
+                task_list.append(q)
+        todos_dict[j.get('id')] = task_list
     with open('todo_all_employees.json', mode='w') as f:
         json.dump(todos_dict, f)
